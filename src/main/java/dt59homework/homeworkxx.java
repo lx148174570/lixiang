a808eeb310c0d2fbc5261aaa4bc67291f430e58f
Administrator <Administrator@E62DXHS1US1BMZI>
2017-12-24 08:36:14 +0800

    李翔的作业

--- src/main/java/dt59homework/homeworkxx.java
+++ src/main/java/dt59homework/homeworkxx.java
@@ -3,9 +3,13 @@ package dt59homework;
 public class homeworkxx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int speed =10;
+		if(speed<=40){
+		 System.out.println(" 中速" );
+		}else if(speed<=20){
+		 System.out.println("低速" );
+		}else if(speed>=60){
+		 System.out.println("高速" );
+		}
 	}
-	
-
 }
